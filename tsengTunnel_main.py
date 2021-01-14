85d256e7c6f5335681f73bdfb78f2d9d0215796c
Johnson Ho <j3ho@yahoo.com>
2021-01-15 00:05:34 +0800

    Add main file

---
+++ tsengTunnel_main.py
@@ -0,0 +1,9 @@
+from engines import std_dev_engine
+
+def main():
+    print ("Tseng Tunel Program Started")
+    std_dev_engine.calc_std_dev()
+
+#Core Step
+# 1. Trigger Standard Deviation Engine
+main()
