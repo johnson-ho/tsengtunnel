3d1330fbe8becbf3cc9a16411113ebc2c16bad9b
Johnson Ho <j3ho@yahoo.com>
2021-01-16 01:21:42 +0800

    Updated 1) main 2) config 3) engine

--- engines/std_dev_engine.py
+++ engines/std_dev_engine.py
@@ -1,3 +1,7 @@
 def calc_std_dev():
-    print("Calculate Standard Deviation")
-
+    print("Calculate Standard Deviation started")
+    f = open("/Users/Johnson/tsengtunnel/data/HSI_closing.csv","r")
+    for x in f:
+        print(x)    
+    f.close()
+    print("Calculate Standard Deviation finish")
--- tsengTunnel_main.py
+++ tsengTunnel_main.py
@@ -1,7 +1,11 @@
 from engines import std_dev_engine
 
+def init():
+    print("Initialize configuration")
+
 def main():
     print ("Tseng Tunel Program Started")
+    init()
     std_dev_engine.calc_std_dev()
 
 #Core Step
